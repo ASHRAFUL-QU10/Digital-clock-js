268b3ecd744b7ef7cc26f99569558f945c350f21
MUHAMMAD ASHRAFUL <106703215+MUHAMMAD-ASHRAFUL-QU10@users.noreply.github.com>
2023-02-10 12:09:28 +0600

    Update DigitalClock.js

--- DigitalClock.js
+++ DigitalClock.js
@@ -3,19 +3,15 @@ const clockElement = document.getElementById('clock');
 function updateClock() {
   
   let time = new Date();
-
   // Extract the hours, minutes, and seconds from the time
   let hours = time.getHours();
   let minutes = time.getMinutes();
   let seconds = time.getSeconds();
-
   // Add leading zeros to the minutes and seconds if necessary
   minutes = (minutes < 10) ? '0' + minutes : minutes;
   seconds = (seconds < 10) ? '0' + seconds : seconds;
-
   // Compose the string for the clock
   let clockString = hours + ':' + minutes + ':' + seconds;
-
   // Update the clock
   clockElement.innerHTML = clockString;
 }
